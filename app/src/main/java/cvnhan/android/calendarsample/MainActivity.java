124fed4499dd9b8e135f928beb1fc8682ca8b572
cvnhan <caovannhan2002@gmail.com>
2015-06-04 17:58:48 +0700

    no message

--- app/src/main/java/cvnhan/android/calendarsample/MainActivity.java
+++ app/src/main/java/cvnhan/android/calendarsample/MainActivity.java
@@ -3,18 +3,17 @@ package cvnhan.android.calendarsample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
-import cvnhan.android.calendarsample.interactivechart.InteractiveLineGraphView;
+import cvnhan.android.calendarsample.calendar.Calendar;
 
 
 public class MainActivity  extends Activity {
-    private InteractiveLineGraphView mGraphView;
+    private Calendar mGraphView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mGraphView = (InteractiveLineGraphView) findViewById(R.id.chart);
+        setContentView(R.layout.activity_calendar);
+        mGraphView = (Calendar) findViewById(R.id.chart);
     }
 
     @Override
@@ -24,34 +23,5 @@ public class MainActivity  extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_zoom_in:
-                mGraphView.zoomIn();
-                return true;
 
-            case R.id.action_zoom_out:
-                mGraphView.zoomOut();
-                return true;
-
-            case R.id.action_pan_left:
-                mGraphView.panLeft();
-                return true;
-
-            case R.id.action_pan_right:
-                mGraphView.panRight();
-                return true;
-
-            case R.id.action_pan_up:
-                mGraphView.panUp();
-                return true;
-
-            case R.id.action_pan_down:
-                mGraphView.panDown();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
